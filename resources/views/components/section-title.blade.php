1bf933abe2c0648fba94d342cc009202fcaf5df6
Dussan Torres <95147426+DNTorr95@users.noreply.github.com>
2023-04-24 19:45:41 -0400

    xzc
    as

--- resources/views/components/section-title.blade.php
+++ resources/views/components/section-title.blade.php
@@ -2,9 +2,9 @@
     <div class="px-4 sm:px-0">
         <h3 class="text-lg font-medium text-gray-900">{{ $title }}</h3>
 
-        <p class="mt-1 text-sm text-gray-600">
+      <!--  <p class="mt-1 text-sm text-gray-600">
             {{ $description }}
-        </p>
+        </p>-->
     </div>
 
     <div class="px-4 sm:px-0">
